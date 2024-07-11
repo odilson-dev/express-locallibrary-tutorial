6527873c21ae4dc83c46a480d53ea0a5e4167e68
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2024-07-10 22:24:35 -0400

    Update user.js file

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
+// Import the mongoose module
+const mongoose = require("mongoose");
+
 var app = express();
 
 // view engine setup
@@ -22,9 +25,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-app.get("/url", (req, res, next) => {
-  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
-});
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set("strictQuery", false);
+
+// Define the database URL to connect to.
+// const mongoDB = "mongodb://127.0.0.1/my_database";
+
+// // Wait for database to connect, logging an error if there is a problem
+// main().catch((err) => console.log(err));
+// async function main() {
+//   await mongoose.connect(mongoDB);
+// }
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
