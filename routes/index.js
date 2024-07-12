dcf1d33a07ee3aef8886980e4467738d4c402201
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2024-07-11 23:19:20 -0400

    Set up all routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Node JS" });
-});
 
+router.get("/", function (req, res) {
+  res.redirect("/catalog");
+});
 
 module.exports = router;
