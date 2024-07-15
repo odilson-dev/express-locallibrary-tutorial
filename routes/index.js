77c718a5bae6432f646c48ebd0a1b3a3cb422581
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2024-07-15 13:11:33 -0400

    Switch to jade views engine

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* GET home page. */
 
 router.get("/", function (req, res) {
-  res.redirect("/catalog");
+  res.render("layout", { title: "Hey", message: "Hello there!" });
+
+  // res.redirect("/catalog");
 });
 
 module.exports = router;
