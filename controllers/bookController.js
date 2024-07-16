26c1345d2123050c3ec9cd9701a120cae9deb176
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2024-07-16 12:31:07 -0400

    Display all books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -33,7 +33,12 @@ exports.index = asyncHandler(async (req, res, next) => {
 
 // Display list of all books.
 exports.book_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book list");
+  const allBooks = await Book.find({}, "title author")
+    .sort({ title: 1 })
+    .populate("author")
+    .exec();
+
+  res.render("book_list", { title: "Book List", book_list: allBooks });
 });
 
 // Display detail page for a specific book.
